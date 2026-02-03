ab88fbacc43aeac217f9bb014d9d1a4db0050a69
Ihwan Ariga <ihwanariga.ia@gmail.com>
2026-02-03 20:51:10 +0700

    membuat role midelware

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  mixed  ...$roles  (role yang diizinkan)
+     */
+    public function handle(Request $request, Closure $next, ...$roles): Response
+    {
+        $user = Auth::user();
+
+        // jika tidak login
+        if (!$user) {
+            return redirect()->route('login');
+        }
+
+        // cek role user
+        if (!in_array($user->role, $roles)) {
+            abort(403, 'Anda tidak memiliki akses ke halaman ini.');
+        }
+
+        return $next($request);
+    }
+}
